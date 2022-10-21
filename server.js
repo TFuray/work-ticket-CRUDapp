df9e2706d312b39957d39f871b9b53006f682da7
TFuray <pk.master881@gmail.com>
2022-10-21 00:08:22 -0700

    initial upload

--- server.js
+++ server.js
@@ -1,2 +1,7 @@
-const express = require('express')
+const express = require("express")
 const app = express()
+const bodyParser = require("body-parser")
+const MongoClient = require("mongodb").MongoClient
+
+
+const connectionString = "mongodb+srv://tfuray:<password>l5VSNKDJ8tyl6WsD@100devs.g4m5nvq.mongodb.net/?retryWrites=true&w=majority"
